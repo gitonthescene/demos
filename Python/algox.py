59f8c83f2c8d36f80bbda9d7ae4430f66fd6ec8b
Douglas Mennella <douglas.mennella@gmail.com>
2020-04-06 09:15:22 +0900

    Use interface for creating columns in the example.  Format with yapf

--- Python/algox.py
+++ Python/algox.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 
 from functools import reduce
-from pprint import pprint as pp
+
 
 class DoulbleLinkedNode:
-    def __init__( self, me=None ):
+
+    def __init__(self, me=None):
         self.me = self if me is None else me
         self.nxt = self
         self.prv = self
 
-    def addAfter( self, ins ):
+    def addAfter(self, ins):
         nxt = self.nxt
         ins.prv = self
         ins.nxt = nxt
@@ -17,111 +18,117 @@ class DoulbleLinkedNode:
         nxt.prv = ins
         return ins.me
 
-    def removeMe( self ):
+    def removeMe(self):
         nxt = self.nxt
         prv = self.prv
         nxt.prv = prv
         prv.nxt = nxt
 
-    def reinsertMe( self ):
+    def reinsertMe(self):
         nxt = self.nxt
         prv = self.prv
         nxt.prv = self
         prv.nxt = self
 
-    def iterOver( self, cb ):
+    def iterOver(self, cb):
         curr = self
         out = []
         while True:
-            out.append( curr )
-            cb( curr.me )
+            out.append(curr)
+            cb(curr.me)
             curr = curr.nxt
             if curr in out:
                 break
 
+
 class QuadLinkedNode:
-    def __init__( self ):
+
+    def __init__(self):
         self.leftright = DoulbleLinkedNode(self)
         self.updown = DoulbleLinkedNode(self)
 
-    def addAfter( self, ins ):
+    def addAfter(self, ins):
         return self.leftright.addAfter(ins.leftright)
 
-    def addBelow( self, ins ):
+    def addBelow(self, ins):
         return self.updown.addAfter(ins.updown)
 
-    def removeMeAcross( self ):
+    def removeMeAcross(self):
         self.leftright.removeMe()
 
-    def removeMeDown( self ):
+    def removeMeDown(self):
         self.updown.removeMe()
 
-    def reinsertMeAcross( self ):
+    def reinsertMeAcross(self):
         self.leftright.reinsertMe()
 
-    def reinsertMeDown( self ):
+    def reinsertMeDown(self):
         self.updown.reinsertMe()
 
-    def iterAcross( self, cb ):
-        self.leftright.iterOver( cb )
+    def iterAcross(self, cb):
+        self.leftright.iterOver(cb)
 
-    def iterDown( self, cb ):
-        self.updown.iterOver( cb )
+    def iterDown(self, cb):
+        self.updown.iterOver(cb)
 
     @property
-    def nxt( self ):
+    def nxt(self):
         return self.leftright.nxt.me
 
     @property
-    def prv( self ):
+    def prv(self):
         return self.leftright.prv.me
 
     @property
-    def dwn( self ):
+    def dwn(self):
         return self.updown.nxt.me
 
     @property
-    def up( self ):
+    def up(self):
         return self.updown.prv.me
 
-class ColHead( QuadLinkedNode ):
-    def __init__( self, nm ):
+
+class ColHead(QuadLinkedNode):
+
+    def __init__(self, nm):
         super().__init__()
         self.nm = nm
         self.cnt = 0
 
-    def addBelow( self, ins ):
-        super().addBelow( ins )
+    def addBelow(self, ins):
+        super().addBelow(ins)
         self.cnt += 1
 
-    def removeMeDown( self ):
+    def removeMeDown(self):
         super().removeMeDown()
         self.cnt -= 1
 
-    def reinsertMeDown( self ):
+    def reinsertMeDown(self):
         super().reinsertMeDown()
         self.cnt += 1
 
 
-class Entry( QuadLinkedNode ):
-    def __init__( self, colhd, row ):
+class Entry(QuadLinkedNode):
+
+    def __init__(self, colhd, row):
         super().__init__()
         self.colhd = colhd
         self.row = row
 
-    def addBelow( self, ins ):
+    def addBelow(self, ins):
         self.colhd.cnt += 1
-        super().addBelow( ins )
+        super().addBelow(ins)
 
-    def removeMeDown( self ):
+    def removeMeDown(self):
         super().removeMeDown()
         self.colhd.cnt -= 1
 
-    def reinsertMeDown( self ):
+    def reinsertMeDown(self):
         super().reinsertMeDown()
         self.colhd.cnt += 1
 
-def cover( col ):
+
+def cover(col):
     col.removeMeAcross()
     row = col.dwn
     while row != col:
@@ -131,7 +138,8 @@ def cover( col ):
             e = e.nxt
         row = row.dwn
 
-def uncover( col ):
+
+def uncover(col):
     row = col.up
     while row != col:
         e = row.prv
@@ -142,39 +150,44 @@ def uncover( col ):
 
     col.reinsertMeAcross()
 
-def pickRow( e ):
+
+def pickRow(e):
     entry = e
     while True:
-        cover( entry.colhd )
+        cover(entry.colhd)
         entry = entry.nxt
         if entry == e:
             break
 
-def rejectRow( e ):
+
+def rejectRow(e):
     entry = e.prv
     while True:
-        uncover( entry.colhd )
+        uncover(entry.colhd)
         entry = entry.prv
         if entry == e.prv:
             break
 
-def buildColumnHeads( colnms ):
+
+def buildColumnHeads(colnms):
     '''
     Helper function to build linked column headers with a dictionary into them by name
     '''
-    colheads = reduce( lambda l, nm: [ l[-1].addAfter( ColHead(nm) ) ]+l, colnms, [ColHead( "::head::" )] )
+    colheads = reduce(lambda l, nm: [l[-1].addAfter(ColHead(nm))] + l, colnms,
+                      [ColHead("::head::")])
     header = colheads.pop()
 
-    constraintDict = dict( zip( colnms, reversed(colheads) ) )
+    constraintDict = dict(zip(colnms, reversed(colheads)))
     return header, constraintDict
 
-def algox( hdr ):
+
+def algox(hdr):
     '''
     Given an appropriately built grid, search for solution.
     Returns False if no such solution exists or the list of rows if one does.
     '''
     out = []
-    hdr.nxt.iterAcross( lambda nd: out.append( nd.cnt ) )
+    hdr.nxt.iterAcross(lambda nd: out.append(nd.cnt))
 
     if hdr.nxt == hdr:
         return True
@@ -188,41 +201,42 @@ def algox( hdr ):
     while True:
         if row == pick:
             return False
-        pickRow( row )
-        res = algox( hdr )
+        pickRow(row)
+        res = algox(hdr)
         if res == True:
             return [row.row]
         elif res:
-            return [row.row]+res
-        rejectRow( row )
+            return [row.row] + res
+        rejectRow(row)
         row = row.dwn
 
+
 def main():
-    grid = [[0,0,1,0,1,1,0],
-            [1,0,0,1,0,0,1],
-            [0,1,1,0,0,1,0],
-            [1,0,0,1,0,0,0],
-            [0,1,0,0,0,0,1],
-            [0,0,0,1,1,0,1]]
+    # 0 0 1 0 1 1 0
+    # 1 0 0 1 0 0 1
+    # 0 1 1 0 0 1 0
+    # 1 0 0 1 0 0 0
+    # 0 1 0 0 0 0 1
+    # 0 0 0 1 1 0 1
 
-    last = reduce( lambda l, nm: l.addAfter( ColHead(nm) ), "ABCDEFG", ColHead( "head" ) )
-    header = last.nxt
+    grid = ["CEF", "ADG", "BCF", "AD", "BG", "DEG"]
 
-    for i,row in enumerate(grid):
-        col = header.nxt
+    header, colDict = buildColumnHeads("ABCDEFG")
+    for row in grid:
         prev = None
-        for vl in row:
-            if vl:
-                e = Entry(col, i)
-                col.up.addBelow(e)
-                if prev:
-                    prev.addAfter(e)
-                prev = e
+        for nm in row:
+            col = colDict[nm]
+            e = Entry(col, row)
+            col.up.addBelow(e)
+            if prev:
+                prev.addAfter(e)
+            prev = e
             col = col.nxt
 
-    print( "AlgoX" )
-    res = algox( header )
-    pp( [grid[row] for row in res], width = 30 )
+    print("AlgoX")
+    res = algox(header)
+    print("\n".join(res))
+
 
 if __name__ == '__main__':
     main()
